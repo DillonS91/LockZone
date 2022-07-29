ec10c6545e6bd0bd43fae8ea0f7f07059f7d7c85
Dillon Shomaker <dcshomaker@gmail.com>
2022-07-29 16:46:53 -0500

    added basic table to Home page (not fully set up yet)

--- LockZone-Frontend/react-lockzone/react-lockzone/src/pages/Home.jsx
+++ LockZone-Frontend/react-lockzone/react-lockzone/src/pages/Home.jsx
@@ -1,9 +1,9 @@
+import { DataReturn } from "../component";
+
 // This is the Home page, will be directed here after login. Can access other poages from home
 
 export const Home = () => {
-    return(
-        <>
-            <h1>LockZone</h1>
-        </>
+    return(   
+        <DataReturn/>
     );
 }
