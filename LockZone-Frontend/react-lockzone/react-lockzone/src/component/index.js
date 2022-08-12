356b37cf16f9e9e9486831d3fb9e6435a923aa15
Dillon Shomaker <dcshomaker@gmail.com>
2022-08-12 15:56:33 -0500

    refinde update page to have minimal code and call the EditWebsite component to handle it

--- LockZone-Frontend/react-lockzone/react-lockzone/src/component/index.js
+++ LockZone-Frontend/react-lockzone/react-lockzone/src/component/index.js
@@ -12,3 +12,4 @@ export * from './Website/WebsiteComponent';
 export * from './Website/AddWebsite';
 export * from './Master/MasterComponent';
 export * from './Accounts/AccountsComponent';
+export * from './Website/EditWebsite';
--- LockZone-Frontend/react-lockzone/react-lockzone/src/pages/Update.jsx
+++ LockZone-Frontend/react-lockzone/react-lockzone/src/pages/Update.jsx
@@ -1,7 +1,8 @@
-import axios from 'axios';
+//import axios from 'axios';
 import { useRef, useState, useEffect } from 'react';
-import { Button, Form, Container, Row, Col, InputGroup } from 'react-bootstrap';
-import { useParams, useLocation, useNavigate } from 'react-router-dom';
+//import { Button, Form, Container, Row, Col, InputGroup } from 'react-bootstrap';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { EditWebsite } from '../component';
 
 
 export const Update = () => {
@@ -18,47 +19,52 @@ export const Update = () => {
         }
     },[]);
 
-    const navigate = useNavigate();
-    const handleSubmit = async (event) => {              
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        setValidated(true);
-
-        try {
-            axios.put(`http://localhost:8080/websites/${locationState.webId}`,
-                {
-                    urlName: urlNameRef.current.value,
-                    master: {
-                        masterId: locationState.masterId
-                    }
-                }
-            )
-        } catch (err) {
-            console.error(err);
-        } finally{
-            navigate('/home');
-        }
-    }
-
     return(
-        <Container>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                <Row className="mb-3">
-                    <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-                        <Form.Label>Website Update</Form.Label>
-                            <InputGroup hasValidation>
-                                <Form.Control type="text" placeholder="Enter a new Website here" name = 'urlName' ref = {urlNameRef} required/>
-                                <Form.Control.Feedback type="invalid">
-                                    Please provide a new website.
-                                </Form.Control.Feedback>
-                            </InputGroup>
-                        </Form.Group>
-                </Row>
-                <Button type="submit">Update</Button>
-            </Form>
-        </Container>
+        <EditWebsite locationState ={locationState}/>
     );
+
+
+    // const navigate = useNavigate();
+    // const handleSubmit = async (event) => {              
+    //     const form = event.currentTarget;
+    //     if (form.checkValidity() === false) {
+    //         event.preventDefault();
+    //         event.stopPropagation();
+    //     }
+    //     setValidated(true);
+
+    //     try {
+    //         axios.put(`http://localhost:8080/websites/${locationState.webId}`,
+    //             {
+    //                 urlName: urlNameRef.current.value,
+    //                 master: {
+    //                     masterId: locationState.masterId
+    //                 }
+    //             }
+    //         )
+    //     } catch (err) {
+    //         console.error(err);
+    //     } finally {
+    //         navigate('/home');
+    //     }
+    // }
+
+    // return(
+    //     <Container>
+    //         <Form noValidate validated={validated} onSubmit={handleSubmit}>
+    //             <Row className="mb-3">
+    //                 <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+    //                     <Form.Label>Website Update</Form.Label>
+    //                         <InputGroup hasValidation>
+    //                             <Form.Control type="text" placeholder="Enter a new Website here" name = 'urlName' ref = {urlNameRef} required/>
+    //                             <Form.Control.Feedback type="invalid">
+    //                                 Please provide a new website.
+    //                             </Form.Control.Feedback>
+    //                         </InputGroup>
+    //                     </Form.Group>
+    //             </Row>
+    //             <Button type="submit">Update</Button>
+    //         </Form>
+    //     </Container>
+    // );
 }
